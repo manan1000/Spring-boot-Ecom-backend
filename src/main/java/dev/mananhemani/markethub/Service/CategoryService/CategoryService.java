c36c908713208e67126046d477c5e649f0220a05
Manan Hemani <mananhemani29@gmail.com>
2024-06-26 21:28:59 +0530

    Created project for an ecommerce

--- src/main/java/dev/mananhemani/markethub/Service/CategoryService/CategoryService.java
+++ src/main/java/dev/mananhemani/markethub/Service/CategoryService/CategoryService.java
@@ -13,5 +13,5 @@ public interface CategoryService {
 
     String deleteCategory(Long categoryId);
 
-    Category updateCategory(Category category, Long categoryId);
+    void updateCategory(Category category, Long categoryId);
 }
