337148c048aa9c0a939e4992b0751dfd94178fe6
manan1000 <mananhemani29@gmail.com>
2024-06-27 00:51:03 +0530

    added global exception handler

--- src/main/java/dev/mananhemani/markethub/Exceptions/MyGlobalExceptionHandler.java
+++ src/main/java/dev/mananhemani/markethub/Exceptions/MyGlobalExceptionHandler.java
@@ -2,13 +2,10 @@ package dev.mananhemani.markethub.Exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 @RestControllerAdvice
