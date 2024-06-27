f9f89f843b197c5f8588e0116462175fb60f6925
manan1000 <mananhemani29@gmail.com>
2024-06-28 01:47:11 +0530

    implementing sorting to categories

--- src/main/java/dev/mananhemani/markethub/Services/CategoryService/CategoryService.java
+++ src/main/java/dev/mananhemani/markethub/Services/CategoryService/CategoryService.java
@@ -6,7 +6,7 @@ import dev.mananhemani.markethub.DTOs.Category.CategoryResponse;
 
 public interface CategoryService {
 
-    CategoryResponse getAllCategories(Integer pageNumber,Integer pageSize);
+    CategoryResponse getAllCategories(Integer pageNumber,Integer pageSize,String sortBy,String sortOrder);
 
     CategoryDTO createCategory(CategoryDTO categoryDTO);
 
