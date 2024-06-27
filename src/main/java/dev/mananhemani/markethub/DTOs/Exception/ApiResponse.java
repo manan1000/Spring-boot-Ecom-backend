bbb8ccc1b8bf1a7ada67ce2829cade74f7798beb
manan1000 <mananhemani29@gmail.com>
2024-06-28 01:55:24 +0530

    standardizing exception response

---
+++ src/main/java/dev/mananhemani/markethub/DTOs/Exception/ApiResponse.java
@@ -0,0 +1,13 @@
+package dev.mananhemani.markethub.DTOs.Exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+    private String message;
+    private boolean status;
+}
--- src/main/java/dev/mananhemani/markethub/Exceptions/MyGlobalExceptionHandler.java
+++ src/main/java/dev/mananhemani/markethub/Exceptions/MyGlobalExceptionHandler.java
@@ -1,29 +1,24 @@
 package dev.mananhemani.markethub.Exceptions;
 
+import dev.mananhemani.markethub.DTOs.Exception.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Objects;
 
 @RestControllerAdvice
 public class MyGlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> methodArgumentNotValidException(MethodArgumentNotValidException e){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Objects.requireNonNull(e.getFieldError()).getDefaultMessage());
-    }
-
-
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> resourceNotFoundException(ResourceNotFoundException e){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ResponseEntity<ApiResponse> resourceNotFoundException(ResourceNotFoundException e){
+        ApiResponse apiResponse = new ApiResponse(e.getMessage(), false);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiResponse);
     }
 
     @ExceptionHandler(ApiException.class)
-    public ResponseEntity<String> apiException(ApiException e){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    public ResponseEntity<ApiResponse> apiException(ApiException e){
+        ApiResponse apiResponse = new ApiResponse(e.getMessage(), false);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiResponse);
     }
 }
