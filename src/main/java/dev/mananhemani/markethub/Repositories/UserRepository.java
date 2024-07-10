9a1331a655d32a145b1517dd037b1883f9dfa0a9
manan1000 <mananhemani29@gmail.com>
2024-07-11 00:29:03 +0530

    implement custom userDetails, userDetailsService and security config

---
+++ src/main/java/dev/mananhemani/markethub/Repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package dev.mananhemani.markethub.Repositories;
+
+import dev.mananhemani.markethub.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String username);
+}
---
+++ src/main/java/dev/mananhemani/markethub/Security/Services/UserDetailsServiceImplementation.java
@@ -0,0 +1,27 @@
+package dev.mananhemani.markethub.Security.Services;
+
+import dev.mananhemani.markethub.Models.User;
+import dev.mananhemani.markethub.Repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserDetailsServiceImplementation implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with username "+username+" not found"));
+
+        return UserDetailsImplementation.build(user);
+    }
+}
